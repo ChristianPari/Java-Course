6934ad39d7b4ca042bb21320990ff005c55d8e4a
Christian Pari <ChristianPari@outlook.com>
2020-10-08 17:32:19 -0400

    Added error handling so that the calculations must have correct values from the user, otherwise the user cannot continue until valid input

--- Project1-Mortgage-Calculator/src/App.java
+++ Project1-Mortgage-Calculator/src/App.java
@@ -11,21 +11,29 @@ public class App {
     // n = Number of Payments; we will be doing years for period so multiply this period by 12; result is n
     // use Math.pow() to use ^n
 
+    Scanner scanner = new Scanner(System.in);
+
     final byte PERCENT = 100;
     final byte MONTHS_IN_YEAR = 12;
 
-    Scanner scanner = new Scanner(System.in);
+    int principal = getPrincipal(scanner);
+    // another way
+    /*
+    int principal = 0;
+    while (true) {
+      System.out.print("Principal: ");
+      principal = scanner.nextInt();
+      if (principal >= 1_000 && principal <= 1_000_000)
+        break;
+      System.out.println("Enter a value between 1000 and 1000000")
+    }
+    */
 
-    System.out.print("Principal: ");
-    int principal = scanner.nextInt();
-
-    System.out.print("Annual Interest Rate: ");
-    float annualRate = scanner.nextFloat();
+    // greater than 0 or less than or equal to 30
+    float annualRate = getAnnualRate(scanner);
     float monthlyRate = annualRate / PERCENT / MONTHS_IN_YEAR;
-    System.out.println(monthlyRate);
 
-    System.out.print("Period (Years): ");
-    byte years = scanner.nextByte();
+    byte years = getYears(scanner);
     int numberOfPayments = years * MONTHS_IN_YEAR;
     
     double mortgage = principal
@@ -63,5 +71,44 @@ public class App {
     // by (int)x we are explicitly converting the x variable into an integer from a double
 
     // to convert a string number to a number you must use the correct wrapper class and then it's method parse"wrapperClassNameHere"(value)
+  }
+
+  public static int getPrincipal(Scanner scanner) {
+
+    System.out.print("Principal ($1K - $1M): ");
+    int principal = scanner.nextInt();
+
+    if (principal >= 1_000 && principal <= 1_000_000) {
+      return principal;
+    } else {
+      System.out.println("Enter a value between 1000 and 1000000");
+      return getPrincipal(scanner);
+    }
+  }
+
+  public static float getAnnualRate(Scanner scanner) {
+
+    System.out.print("Annual Interest Rate: ");
+    float annualRate = scanner.nextFloat();
+
+    if (annualRate >= 1 && annualRate <= 30) {
+      return annualRate;
+    } else {
+      System.out.println("Enter a value greater than 0 and less than or equal to 30");
+      return getAnnualRate(scanner);
+    }
+  }
+
+  public static byte getYears(Scanner scanner) {
+  
+    System.out.print("Period (Years): ");
+    byte years = scanner.nextByte();
+
+    if (years >= 1 && years <= 30) {
+      return years;
+    } else {
+      System.out.println("Enter a value greater than 0 and less than or equal to 30");
+      return getYears(scanner);
+    }
   }
 }
