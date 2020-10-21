cbcdbe538d1ebfcee8bedb95a3cec40e8be0d6d0
Christian Pari <ChristianPari@outlook.com>
2020-10-20 21:19:28 -0400

    Continuing work w/ generics, comparable interface and mutliple type parameters

--- AdvancedSection/src/com/christianpari/generics/GenMain.java
+++ AdvancedSection/src/com/christianpari/generics/GenMain.java
@@ -5,6 +5,21 @@ public class GenMain {
 //    GenericList<Integer> numbers = new GenericList<>();
 //    numbers.add(1); // Boxing
 //    int number = numbers.get(0); // Unboxing
-    new GenericList<User>();
+
+//    User user1 = new User(10);
+//    User user2 = new User(10);
+//    if (user1.compareTo(user2) < 0)
+//      System.out.println("User1 < User2");
+//    else if (user1.compareTo(user2) == 0)
+//      System.out.println("User1 = User2");
+//    else
+//      System.out.println("User1 > User2");
+
+//    User max = Utils.max(new User(10), new User(20));
+//    System.out.println(max);
+//    Utils.print(1, "Chris");
+
+    User user = new Instructor(10);
+    Utils.printUser(user);
   }
 }
--- AdvancedSection/src/com/christianpari/generics/User.java
+++ AdvancedSection/src/com/christianpari/generics/User.java
@@ -1,5 +1,25 @@
 package com.christianpari.generics;
 
-public class User{
+public class User implements Comparable<User>{
+  private int points;
 
+  public User(int points) {
+    this.points = points;
+  }
+
+  @Override
+  public int compareTo(User other) {
+//    this < other => negative value
+//    this == other => 0
+//    this > other => positive value
+//    if (points < other.points) return -1;
+//    if (points == other.points) return 0;
+//    return 1;
+    return points - other.points;
+  }
+
+  @Override
+  public String toString() {
+    return "Points=" + points;
+  }
 }
