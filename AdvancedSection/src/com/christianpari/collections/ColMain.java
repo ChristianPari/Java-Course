f7bf461bc19bbedaf3bef7a9481b2fa49c99b763
Christian Pari <ChristianPari@outlook.com>
2020-10-24 20:05:36 -0400

    Understanding Sets and the operations behind making them

--- AdvancedSection/src/com/christianpari/collections/ColMain.java
+++ AdvancedSection/src/com/christianpari/collections/ColMain.java
@@ -6,12 +6,6 @@ import java.util.List;
 
 public class ColMain {
   public static void main(String[] args) {
-    List<Customer> customers = new ArrayList<>();
-    customers.add(new Customer("b", "e3"));
-    customers.add(new Customer("a", "e2"));
-    customers.add(new Customer("c", "e1"));
-//    Collections.sort(customers);
-    Collections.sort(customers, new EmailComparator());
-    System.out.println(customers);
+    SetDemo.show();
   }
 }
---
+++ AdvancedSection/src/com/christianpari/collections/SetDemo.java
@@ -0,0 +1,28 @@
+package com.christianpari.collections;
+
+import java.util.*;
+
+public class SetDemo {
+//  set interface does not guarantee the order of items
+  public static void show() {
+//    Set<String> set = new HashSet<>();
+//    set.add("sky");
+//    set.add("is");
+//    set.add("blue");
+//    set.add("blue");
+//    System.out.println(set);
+//  prints [sky, blue, is]
+//    Collection<String> collection = new ArrayList<>();
+//    Collections.addAll(collection, "a", "b", "c", "c");
+//    below creates a new set from a list in one line
+//    Set<String> set = new HashSet<>(collection);
+    Set<String> set1 = new HashSet<>(Arrays.asList("a", "b", "c"));
+    Set<String> set2 = new HashSet<>(Arrays.asList("b", "c", "d"));
+    /*
+    Operations...
+    Union - adding one Set to another via addAll()
+    Intersection - by using retainAll() you keep only items that appear in both sets
+    Difference - by using removeAll() you take away everything from the being compared set that is within the comparing set
+     */
+  }
+}
